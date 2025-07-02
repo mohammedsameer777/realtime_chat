258d36825e5d9f7d09b960d1e9bf8ee2cd114a2a
Sameer Mohammed <smohammedsameer0707@gmail.com>
2025-07-02 12:34:35 +0530

    Fix: force name prompt in room.html

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {'room_name': room_name})
+    name = request.GET.get("name", "")  # get name from ?name=User
+    return render(request, "chat/room.html", {
+        "room_name": room_name,
+        "username": name
+    })
